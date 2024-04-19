2e5ee6d4535fc04a366f125d7e32ae30e266f1ce
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:38:08 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_river.mk
+    $(LOCAL_DIR)/lmodroid_river.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_river-eng \
-    lineage_river-user \
-    lineage_river-userdebug
+    lmodroid_river-eng \
+    lmodroid_river-user \
+    lmodroid_river-userdebug
--- lmodroid_river.mk
+++ lmodroid_river.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -11,7 +11,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := river
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := moto g(7)
-PRODUCT_NAME := lineage_river
+PRODUCT_NAME := lmodroid_river
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=river_retail \
